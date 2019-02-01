445f8fe3d35dacf018a4d3a569154d32e94f269d
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2019-02-01 02:23:16 +0000

    added csv output

--- src/app.js
+++ src/app.js
@@ -1,13 +1,53 @@
 'use strict';
 const service = require('./services/thinkmate')
 const url = require('url')
+const RSVP = require('rsvp')
+const _ = require('lodash')
+const format = require('util').format
 
 let link = process.argv[2] || process.exit(1);
 
-//service.getPageCount(url.parse(link)).then(count => console.log(count))
-
-//service.getProductLinksOnPage(url.parse(link)).then(links => links.map((link) => console.log(link)))
-
-// ssd keys i would like in a report -- price is always given
+// custom keys per product type
 let ssdKeys = ["Product Type", "Storage Capacity", "Form Factor", "Interface", "Series", "Lifetime Endurance", "Life Expectancy", "Read IOPS", "Write IOPS", "Read Speed", "Write Speed", "NAND"]
-service.getProductDetailsOnPage(url.parse(link), ssdKeys).then(d => console.log(d))
+
+service.getPageCount(url.parse(link)).then(count => {
+	let pageLinks = []
+	for (let i = 1; i <= 1; i++) {
+		pageLinks.push(format("%s?p=%s", link, i))
+	}
+	return pageLinks
+}).then(pageLinks => {
+	let promises = []
+	pageLinks.forEach(pageLink => {
+		let p = service.getProductLinksOnPage(url.parse(pageLink))
+		promises.push(p)
+	})
+	return RSVP.all(promises).then(result => _.flatten(result))
+}).then(productLinks => {
+	let promises = []
+	productLinks.forEach(productLink => {
+		// console.log(productLink)
+		let p = service.getProductDetailsOnPage(url.parse(productLink), ssdKeys)
+		promises.push(p)
+	})
+	return RSVP.all(promises)
+}).then(productDetails => {
+	let header = []
+	for (let h of ssdKeys) {
+		header.push(format("\"%s\"", h))
+	}
+	console.log(header.join(","))
+	productDetails.forEach(product => {
+		let out = []
+		for (let key of ssdKeys) {
+			out.push(format("\"%s\"", product[key]))	
+		}
+		console.log(out.join(","))
+	})
+})
+
+/*
+let ssdKeys = ["Product Type", "Storage Capacity", "Form Factor", "Interface", "Series", "Lifetime Endurance", "Life Expectancy", "Read IOPS", "Write IOPS", "Read Speed", "Write Speed", "NAND"]
+service.getProductDetailsOnPage(url.parse(link), ssdKeys).then((d)=> console.log(d))
+// ssd keys i would like in a report -- price is always given
+*/
