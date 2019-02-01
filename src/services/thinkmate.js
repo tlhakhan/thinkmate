48a27517956b825ca66b17a66d258749175b9079
root <root@node-00.cloudcity.local>
2019-02-01 01:38:25 +0000

    update comments for css selectors

--- src/services/thinkmate.js
+++ src/services/thinkmate.js
@@ -7,9 +7,9 @@ const cheerio = require('cheerio')
 let getPageCount = (url) => {
 	return fetch(url.href).then(res => res.text()).then(body => {
 		let $ = cheerio.load(body)
-		// selector - need first() because there is a page count at top and bottom of pages
+		// the last anchor tag contains the the 'last' url link
 		let out = $('.product-pagination a').last().attr('href')
-		// the captured text will be in formation of 1/${count}
+		// the captured out will be a url that can be split on ?p= and grab the second item in the array
 		let count = out.split('?p=')[1]
 		return count
 	})
@@ -20,7 +20,7 @@ let getProductLinksOnPage = (url) => {
 	return fetch(url.href).then(res => res.text()).then(body => {
 		let $ = cheerio.load(body)
 		let links = []
-		// selector - nice name scoping
+		// easy to grab each link in this list view ui
 		$('.table_list .product h2 a').each((i, el) => {
 			links.push($(el).attr('href'))
 		})
@@ -33,7 +33,10 @@ let getProductDetailsOnPage = (url, keys) => {
 	return fetch(url.href).then(res => res.text()).then(body => {
 		let $ = cheerio.load(body)
 		let details = {}	
-		// given the keys to find create a details object that is populated
+		// given the keys array, create a details object that is populated with those keys
+
+		// the kvPairs contains the details table
+		// the array needed to be named because the .each function by cheerio doesn't allow you to reference the array that i'm traversing inside of the each function.
 		let kvPairs = $('.table_compare .details td')
 		kvPairs.each((i, el) =>  {
 			// the keys have the class name 'label'
@@ -41,6 +44,7 @@ let getProductDetailsOnPage = (url, keys) => {
 				let key = $(el).text()
 				// values appear to always be index of label + 1
 				// check for array index out of bounds
+				// i reference the original array inside of a function that's traversing over it
 				let validate = kvPairs[i+1]
 				if (validate) {
 					// label class really has a value next +1 door, now get text
